af8b66ca46c3f553dac9ea0aa2b49999f321828d
Tan Chin Sheng <tanchinsheng@gmail.com>
2015-02-21 20:51:07 +0800

    Added  Consulting Agency

---
+++ ConsultingAgency/src/java/jpa/entities/BillableFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.entities;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author cstan
+ */
+@Stateless
+public class BillableFacade extends AbstractFacade<Billable> {
+    @PersistenceContext(unitName = "ConsultingAgencyPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public BillableFacade() {
+        super(Billable.class);
+    }
+
+}
---
+++ ConsultingAgency/src/java/jpa/entities/RecruiterFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.entities;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author cstan
+ */
+@Stateless
+public class RecruiterFacade extends AbstractFacade<Recruiter> {
+    @PersistenceContext(unitName = "ConsultingAgencyPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public RecruiterFacade() {
+        super(Recruiter.class);
+    }
+
+}
